5b34424589d20b59c611fac0d0e93fde039ba209
Paulina Sadowska <palka@Paulinas-MacBook-Air.local>
2015-11-19 20:58:11 +0100

    Distance sensor implemented. Works fine for distances greater than 10cm.

--- DistanceSensor.c
+++ DistanceSensor.c
@@ -6,77 +6,47 @@
  */
 #include "DistanceSensor.h"
 
-uint32_t  DSDistanceCm = 0;
-uint32_t value0 = 0;
-uint32_t value = 0;
-uint32_t diff[20];
-uint32_t temp = 0;
-uint32_t diffAverage;
-volatile uint32_t InterruptErrorCnt;
-volatile uint32_t Distance;
-volatile uint32_t Overflow = 0;
-volatile uint32_t  i = 0;
-volatile uint8_t Rising = 0x1;
+uint32_t  DSDistancemm = 0;
+
+uint16_t value[2] = {0, 0};
+volatile bool Rising = false;
+
+//Timer2A gets edge interrupt from PB0
 void Timer2AIntHandler(void)
 {
 
-	// save the timer config so we know which edge this is
 	  volatile uint32_t interruptStatus = TimerIntStatus(TIMER2_BASE, false);
 	  TimerIntClear(TIMER2_BASE, interruptStatus);
 
-	  switch(interruptStatus)
+	  if( interruptStatus & TIMER_CAPA_EVENT )
 	  {
-	  case TIMER_TIMA_TIMEOUT:
-		  Overflow++;
-		  break;
-	  case TIMER_CAPA_EVENT:
 		  if(Rising)
 		  {
-			  value = TimerValueGet(TIMER2_BASE, TIMER_A);
-			  Rising ^=0x1;
+			  value[1] = TimerValueGet(TIMER2_BASE, TIMER_B);
+			  Rising = false;;
 		  }
 		  else
 		  {
-			    Rising ^=0x1;
-				value0 = TimerValueGet(TIMER2_BASE, TIMER_A);
-
-				diff[i] = 65535 * Overflow + value0 - value;
-					temp = temp + diff[i];
-					i++;
-					if(i==10)
-					{
-						diffAverage = temp / 10;
-						Distance = (diffAverage * 170) / 40000;
-						temp = 0;
-						Overflow = 0;
-						i = 0;
-					}
-		  }
-		  break;
-	  default:
-		  InterruptErrorCnt++;
-		  break;
+			  	int32_t timeDifference;
+			    Rising = true;
+				value[0] = TimerValueGet(TIMER2_BASE, TIMER_B);
+				timeDifference = value[1] - value[0];
+				if(timeDifference<0){
+					timeDifference += 60000;
+				}
+				DSUpdateDistance(timeDifference);
+		  	}
 	  }
-
-//	  if(interruptStatus & TIMER_TIMA_TIMEOUT)
-//	  {
-//		  // clear our interrupt mask
-//	  }
-//	  else if (interruptStatus & TIMER_EVENT_NEG_EDGE) {
-//		// clear our interrupt mask
-//	    // set to positive edge
-//		    //TimerControlEvent(TIMER2_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
-//	  }
-//	  else
-//	  {
-//		  // clear our interrupt mask
-//	    // Rising edge!
-//	    // set to negative edge
-//	    //TimerControlEvent(TIMER2_BASE, TIMER_A, TIMER_EVENT_NEG_EDGE);
-//	  }
-
 }
 
+//recalciulate timer difference to time and distance
+void DSUpdateDistance(int32_t diff)
+{
+	volatile uint32_t time = (diff * TIMER_B_PRESCALER) / ClockFrequencyMHz; //time ms
+	DSDistancemm = (time * 34) / 200; //distance [mm]
+}
+
+//Timer2B triggers the measurement by setting PB0
 void Timer2BIntHandler(void)
 {
 	  // clear our interrupt mask
@@ -87,16 +57,15 @@ void Timer2BIntHandler(void)
 		  GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_3, 0xFF);
 }
 
-void DSInit(unsigned int frequency)
+void DSInit()
 {
-	DSInitTimer2(frequency);
+	ClockFrequencyMHz = SysCtlClockGet()/1000000; // gets clock frequency in MHz.
+	DSInitTimer2();
 	DSInitGPIO();
-	//IntMasterEnable();  //master interrupt enable API for all interrupts.
 }
 
-void DSInitTimer2(unsigned int frequency)
+void DSInitTimer2()
 {
-	 uint16_t ui16Period; //desired clock period
 	  // Configure PB0 as T2CCP0
 	  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	  GPIOPinConfigure(GPIO_PB0_T2CCP0);
@@ -105,45 +74,35 @@ void DSInitTimer2(unsigned int frequency)
 	  // enable timer 2
 	  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);
 
-	  // timer a is a time capture event timer
+	  // timer A is a time capture event timer, timer B : periodic
 	  TimerConfigure(TIMER2_BASE, TIMER_CFG_SPLIT_PAIR |TIMER_CFG_A_CAP_TIME_UP | TIMER_CFG_B_PERIODIC );
-	  //TimerConfigure(TIMER2_BASE, TIMER_CFG_SPLIT_PAIR | |TIMER_CFG_A_ACT_NONE );
 
-	  /*
-	  * desired frequency = 10Hz
-	  * duty cycle = 50% (interrupt at 1/2 of the desired period)
-	  */
-	  ui16Period = (SysCtlClockGet() / 1000) / 2;
+	  //desired timer period. Set load and prescaler
+	  TimerLoadSet(TIMER2_BASE, TIMER_B, TIMER_B_LOAD); // To = 1.5 ms
+	  TimerPrescaleSet(TIMER2_BASE, TIMER_BOTH, TIMER_B_PRESCALER); //T = 1.5ms * 126 = 190.5ms
 
-	  /*
-	   * load calculated period into the Timer’s Interval Load register using the TimerLoadSet
-	   * you have to subtract one from the timer period since the interrupt fires at the zero count
-	   */
-	  TimerLoadSet(TIMER2_BASE, TIMER_B, 65534);
-	  TimerLoadSet(TIMER2_BASE, TIMER_A, 20534);
-
-	  TimerIntEnable(TIMER2_BASE, TIMER_CAPA_EVENT | TIMER_TIMA_TIMEOUT|TIMER_TIMB_TIMEOUT); 	// interrupt on timer a capture event and timeout
+	  // interrupt on timer a capture event and timeout
+	  TimerIntEnable(TIMER2_BASE, TIMER_CAPA_EVENT | TIMER_TIMB_TIMEOUT);
 
 	  // both edges
 	  TimerControlEvent(TIMER2_BASE, TIMER_A, TIMER_EVENT_BOTH_EDGES);
 
 	  // register our interrupt
 	  TimerIntRegister(TIMER2_BASE, TIMER_A, Timer2AIntHandler);
-	  // register our interrupt
 	  TimerIntRegister(TIMER2_BASE, TIMER_B, Timer2BIntHandler);
 
-	  IntEnable(INT_TIMER2A); // enables the specific vector associated with Timer2A
-	  IntEnable(INT_TIMER2B); // enables the specific vector associated with Timer2B
+	  // enables the specific vector associated with Timer2A / Timer2B
+	  IntEnable(INT_TIMER2A);
+	  IntEnable(INT_TIMER2B);
+
+	  // sets interrupt pririty
 	  IntPrioritySet(INT_TIMER2A,0);
 	  IntPrioritySet(INT_TIMER2B,1);
-
-	  //TimerIntEnable(TIMER2_BASE, TIMER_CAPA_EVENT | TIMER_TIMA_TIMEOUT|TIMER_TIMB_TIMEOUT); 	// interrupt on timer a capture event and timeout
-	  //TimerIntEnable(TIMER2_BASE, TIMER_TIMB_TIMEOUT);  // interrupt on timeout of Timer 2B
 
 	  // int master enable
 	  IntMasterEnable();
 
-	  // and turn it on
+	  //turn timers on
 	  TimerEnable(TIMER2_BASE, TIMER_A);
 	  TimerEnable(TIMER2_BASE, TIMER_B);
 
@@ -151,8 +110,8 @@ void DSInitTimer2(unsigned int frequency)
 
 void DSInitGPIO()
 {
-	SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-	//SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	//PB3 - trigger
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 	GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_3);
+
 }
--- DistanceSensor.h
+++ DistanceSensor.h
@@ -10,37 +10,48 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include <inc/hw_timer.h>
+#include "inc/hw_memmap.h" //memory map
 #include "inc/hw_ints.h" //includes valid INT_ values for IntEnable function parameter
-#include <driverlib/sysctl.h>
-#include "driverlib/gpio.h"
-#include "driverlib/debug.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/timer.h" //macros for Timer API
+#include <driverlib/sysctl.h> //SYSCTL macros
+#include "driverlib/gpio.h"	//GPIO functiuons
+#include "driverlib/interrupt.h"//interrupts functions
+#include "driverlib/pin_map.h" 	//includes GPIO_PB0_T2CCP0 definition
+#include "driverlib/timer.h" 	//macros for Timer API
 
-extern unsigned int DSDistanceCm;
-extern uint32_t value;
+#define TIMER_B_LOAD 60000 // To = 1.5ms dla freq 40Mhz; 1.2ms dla 50Mhz
+#define TIMER_B_PRESCALER 0x7F // 127; 127 * 1.5 = 190.5ms; 127 * 1.2 = 152.4 ms;
+
+
+extern uint32_t  DSDistancemm; ///Value used outside library. Stores measured distance in mm
+
+extern uint16_t value[2]; 	 ///Helper values used to store timer value on both edges of echo signal
+extern volatile bool Rising; ///Flag which determine if edge is rising or falling.
+uint16_t ClockFrequencyMHz;
+
+
+/*
+ * Initialize Distance Sensor
+ * Only function called from outside!!
+ */
+void DSInit();
+
 /* Initialize timer 2A
  * used to trigger distance measurement
  * and timer 2B
  * used to measure time of echo
  * which determine distance
  */
-void DSInitTimer2(unsigned int frequency);
+void DSInitTimer2();
 
 /* Initialize GPIO
  * PB3 - trigger
- * PB4 - echo
  */
 void DSInitGPIO();
+
 /*
- * Initialize Distance Sensor
+ * gets timer value difference and calculates DSDistancemm
  */
-void DSInit(unsigned int frequency);
+void DSUpdateDistance(int32_t diff);
 
 
 #endif /* DISTANCESENSOR_H_ */
